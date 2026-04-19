61da1f65912c5107a29b71a78f9246d36d0f3597
mazyardesigngroup-collab <mazyar.design.group@gmail.com>
2026-04-19 16:09:03 +0330

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ else:
                     """
                     
                     # فراخوانی مدل
-                    model = genai.GenerativeModel("gemini-1.5-flash")
+                    model = genai.GenerativeModel("gemini-1.5-flash-latest")
                     response = model.generate_content(prompt)
                     
                     # نمایش جواب
